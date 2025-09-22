d5863940bf898ebd76ae5a8b8ccd6a0b083171bd
2DPG-E320-05-PC <chajihun1171@gmail.com>
2025-09-22 13:03:47 +0900

    애니메이션 4종 루프 구현

--- animation_viewer.py
+++ animation_viewer.py
@@ -4,6 +4,13 @@ open_canvas()
 
 character = load_image('knight_animation_sheet.png')
 
+def draw_knight(i):
+    if (i == 0): draw_knight_walk()
+    elif (i == 1): draw_knight_run()
+    elif (i == 2): draw_knight_attack()
+    elif (i == 3): draw_knight_runattack()
+    pass
+
 while True:
     for i in range(3):
         draw_knight(i)
