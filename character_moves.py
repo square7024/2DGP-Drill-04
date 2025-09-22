b62645fa40fab1cfa02fb81984f0cc9e5664aa12
2DPG-E320-05-PC <chajihun1171@gmail.com>
2025-09-22 12:26:37 +0900

    수업 시간 내 실습

--- character_moves.py
+++ character_moves.py
@@ -5,7 +5,12 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-# 여기를 채우세요.
+for x in range(0, 800, 2):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, 90)
+    update_canvas()
+    delay(0.01)
 
 close_canvas()
 
--- character_runs.py
+++ character_runs.py
@@ -5,8 +5,14 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
-# fill here
-
+frame = 0
+for x in range(0, 800, 10):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_composite_draw(frame*100, 0, 100, 100, 0, 'h', x, 130, 200, 200)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
 
 close_canvas()
 
